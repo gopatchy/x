c291f597088f8074aa7174786bec71ca55542469
Ian Gulliver <github@flamingcow.io>
2024-11-24 22:05:22 -0600

    PQ test

--- main.go
+++ main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "80"
+		log.Fatalf("please set PORT")
+	}
+
+	pgConn := os.Getenv("PGCONN")
+	if pgConn == "" {
+		log.Fatalf("please set PGCONN")
+	}
+
+	_, err := sql.Open("postgres", pgConn)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	bind := fmt.Sprintf(":%s", port)
